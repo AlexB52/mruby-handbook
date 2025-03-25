7db75aa7f23517aa7441d3cfc5b62e036a985173
Alexandre Barret <barret.alx@gmail.com>
2025-03-25 22:29:18 +1300

    Go through first 4 chapters

---
+++ hello/plus.c
@@ -0,0 +1,23 @@
+#include <mruby.h>
+#include <mruby/compile.h>
+#include <mruby/numeric.h>
+
+int main(void) {
+  mrb_value val;
+  mrb_state *mrb = mrb_open();
+
+  mrb_value n1 = mrb_fixnum_value(12);
+  mrb_value n2 = mrb_fixnum_value(34);
+
+  val = mrb_num_add(mrb, n1, n2);
+  mrb_p(mrb, val);
+
+  val = mrb_num_sub(mrb, n1, n2);
+  mrb_p(mrb, val);
+
+  val = mrb_num_mul(mrb, n1, n2);
+  mrb_p(mrb, val);
+
+  mrb_close(mrb);
+  return 0;
+}
